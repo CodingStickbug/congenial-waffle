af38d829c610746e499795964e9974df6d240368
CodingStickbug <stefangrblr7@gmail.com>
2025-06-06 08:36:39 +0200

    Notes App and Fluttery.
    Lots of Notes app progress, just mentioning fluttery as I'm still working on it.

--- FlutteryStudiery/fluttery/lib/main.dart
+++ FlutteryStudiery/fluttery/lib/main.dart
@@ -4,65 +4,31 @@ void main() {
   runApp(const MyApp());
 }
 
-String name = "mammoth";
-int number = 1;
-double float = 1.25;
-List myList = ["hellow", "world"];
-Map myMap = {"String": "wow"};
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 161, 85, 13),
+          seedColor: Color.fromARGB(211, 187, 117, 12),
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          widget.title,
-          style: TextStyle(color: Color.fromARGB(237, 247, 198, 63)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("The best damned flutter application")),
+          backgroundColor: Color.fromARGB(211, 187, 117, 12),
         ),
-        leading: Icon(Icons.home, color: Color.fromARGB(237, 247, 198, 63)),
-      ),
-      body: Wrap(children: [
-          Center(child: Text("Does he know about the D O R E ?")),
-        ] ,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+            NavigationDestination(icon: Icon(Icons.person), label: "Account"),
+          ],
+          selectedIndex: 1,
+        ),
       ),
     );
   }
--- cedarnie/notesapp/lib/main.dart
+++ cedarnie/notesapp/lib/main.dart
@@ -4,16 +4,53 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isSwitched = true;
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: isSwitched ? ThemeData.dark() : ThemeData.light(),
+      //colorScheme: ColorScheme.fromSeed(
+      //seedColor: Color.fromARGB(186, 230, 164, 22),
+      //brightness: Brightness.dark,
+      //)
+      //),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(206, 201, 133, 8),
-          title: Icon(Icons.note_outlined, size: 50.0),
+          title: Text("Note's App"),
+          leading: Icon(Icons.note_alt, size: 50.0),
+          actions: [
+            Switch(
+              activeColor: Colors.amberAccent,
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = !isSwitched;
+                });
+              },
+            ),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.note_add),
+              label: "New Note",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.edit_note),
+              label: "View Notes",
+            ),
+          ],
         ),
       ),
     );
