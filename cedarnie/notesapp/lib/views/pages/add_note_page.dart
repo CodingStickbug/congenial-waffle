f5b386b40716635a4e70277a03b3afd0e5261549
CodingStickbug <stefangrblr7@gmail.com>
2025-06-11 09:39:46 +0200

    NotesApp completed.
    The notes app has been completed for both desktop and mobile.

--- cedarnie/notesapp/lib/views/pages/add_note_page.dart
+++ cedarnie/notesapp/lib/views/pages/add_note_page.dart
@@ -23,79 +23,81 @@ class _AddNotePageState extends State<AddNotePage> {
           child: Stack(
             children: [
               Center(child: HeroWidget()),
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      "Add Note",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
+              SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        "Add Note",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    SizedBox(
-                      width: 750,
-                      child: TextField(
-                        controller: controllerTitle,
-                        decoration: InputDecoration(
-                          hintText: "Title",
-                          contentPadding: EdgeInsets.all(8.0),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: 750,
+                        child: TextField(
+                          controller: controllerTitle,
+                          decoration: InputDecoration(
+                            hintText: "Title",
+                            contentPadding: EdgeInsets.all(8.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    SizedBox(
-                      width: 750,
-                      child: TextFormField(
-                        controller: controllerNote,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: 13,
-                        decoration: InputDecoration(
-                          hintText: "Note: just start writing...",
-                          contentPadding: EdgeInsets.all(8.0),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: 750,
+                        child: TextFormField(
+                          controller: controllerNote,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 13,
+                          decoration: InputDecoration(
+                            hintText: "Note: just start writing...",
+                            contentPadding: EdgeInsets.all(8.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              backgroundColor: Color.fromARGB(113, 0, 0, 0),
-                              content: Center(child: Text("Note Created!", style: TextStyle(color: Colors.white),))
-                            )
-                          );
-                          boxNotes.put(
-                            "key_${controllerTitle.text}",
-                            Note(
-                              title: controllerTitle.text,
-                              description: controllerNote.text,
-                            ),
-                          );
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return WidgetTree();
-                              },
-                            ),
-                          );
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(500, 50),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                backgroundColor: Color.fromARGB(113, 0, 0, 0),
+                                content: Center(child: Text("Note Created!", style: TextStyle(color: Colors.white),))
+                              )
+                            );
+                            boxNotes.put(
+                              "key_${controllerTitle.text}",
+                              Note(
+                                title: controllerTitle.text,
+                                description: controllerNote.text,
+                              ),
+                            );
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return WidgetTree();
+                                },
+                              ),
+                            );
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(500, 50),
+                        ),
+                        child: Text("Create Note"),
                       ),
-                      child: Text("Create Note"),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
